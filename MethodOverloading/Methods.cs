092ba918572ecd1bebf64e87b041af8658be14ba
Jake Johnston <jakejohnston618@gmail.com>
2023-03-13 19:34:10 -0500

    finished the exercise

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static string Add(bool x, int y, int z)
+        {
+            if (x && (z + y != 1))
+            {
+                return $"{z + y} dollars.";
+            }
+            else if (x && (y + z != 1))
+            {
+                return $"{z + y} dollar.";
+            }
+            else
+            {
+                return $"{z + y}";
+
+            }
+        }
+    }
+}
+
+        
+
+    
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace MethodOverloading
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Methods.Add(6, 8);
+            Console.WriteLine(answer);
         }
+        
+        
     }
 }
